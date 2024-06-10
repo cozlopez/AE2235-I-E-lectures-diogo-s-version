32a3cdd89df9b7a7d42a8f8cbce13ad9644fbc9f
diogocravocosta <diogocravocosta@gmail.com>
2024-06-10 15:56:40 +0200

    End of E-Lecture 6

--- e-lecture 6.py
+++ e-lecture 6.py
@@ -1,7 +1,9 @@
 import numpy as np
 import control.matlab as c
+from control.matlab import poles
 from scipy.linalg import eig
 import matplotlib.pyplot as plt
+import scipy
 #first description
 # a = np.matrix(""" 0       1      0   ;
 #                  -0.0071 -0.111  0.12;
@@ -79,3 +81,60 @@ import matplotlib.pyplot as plt
 # print(output[2][int(10/dt):,1].max())
 
 #state space to transfer function
+
+
+#help(c.rss)
+# A = [ [ -1.4,    0,    0  ],
+#       [ 0.4, -1.2, -5.0],
+#       [ 0,    1,    0  ] ]
+# B = [ [0.3 ], [0 ], [0 ] ]
+# C = [ [0,   1, 0 ],
+#       [0.1, 0, 1 ]]
+# D= [ [0],
+#      [0] ]
+# sys = c.ss(A,B,C,D)
+# H = c.tf(sys)
+# roots = np.roots(H.den[0][0])
+# print(H)
+# print(roots)
+
+#converting from transfer function to state space
+
+# s = c.tf([1,0],[1])
+# b0 = 0.4
+# b1 = 0.1
+# b2 = 0.5
+# a0 = 2.3
+# a1 = 6.3
+# a2 = 3.6
+# a3 = 1.0
+#
+# h = (b0 + b1*s + b2*s**2)/( a0 + a1*s + a2*s**2 + a3*s**3 )
+#
+# H = c.tf([[h.num[0][0]],[(h*s).num[0][0]]],
+#         [[h.den[0][0]],[(h*s).den[0][0]]])
+#
+# print(c.ss(H))
+# print(eig(c.ss(H).A))
+# print(h.poles())
+
+#State-Space Systems Diagram
+
+K1 = 0.6
+K2 = 2.8
+K3 = 2.5
+K4 = 0.6
+
+A = np.matrix([[0, -K4*K2, -K4*K3],
+               [1, -1, 0],
+               [0,1,0]])
+
+B = np.matrix([[K4, 1],
+               [0,0],
+               [0,0]])
+
+C = np.matrix([[K1, 0, 1]])
+
+D = np.matrix([[0,0]])
+
+print(c.ss(A, B, C, D))
